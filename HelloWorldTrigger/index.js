c6b454e6a6db725e932f29c63831ccf7d413aae9
Richard Grand'Maison <rgrandmaison@stack8.com>
2018-04-05 15:40:06 -0400

    1

--- HelloWorldTrigger/index.js
+++ HelloWorldTrigger/index.js
@@ -2,9 +2,11 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
+
+        var lol = 1 + 1;
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name) + ", how are you good sir?"
+            body: "Hello " + (req.query.name || req.body.name) + ", how are you good sir?" + lol
         };
     }
     else {
