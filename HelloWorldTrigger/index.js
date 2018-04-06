2a2ea3dcb3562cd0284d82b2643702f855265acc
Richard Grand'Maison <rgrandmaison@stack8.com>
2018-04-06 10:43:28 -0400

    test

--- HelloWorldTrigger/index.js
+++ HelloWorldTrigger/index.js
@@ -1,20 +1,21 @@
+module.exports = function(context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-
-        var lol = 1 + 1;
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name) + ", how are you good sir?" + lol
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
+  if (req.query.name || (req.body && req.body.name)) {
+    var lol = 1 + 2;
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body:
+        "Hello " +
+        (req.query.name || req.body.name) +
+        ", how are you good sir?" +
+        lol
+    };
+  } else {
+    context.res = {
+      status: 400,
+      body: "Please pass a name on the query string or in the request body"
+    };
+  }
+  context.done();
 };
